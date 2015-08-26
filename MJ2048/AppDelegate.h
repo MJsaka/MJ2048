852c78ce93f392d7f9dcafa12e0384da2f0fe651
MJsaka <mjsaka@MJ-Mac.local>
2015-08-26 16:16:27 +0800

    Initial Commit

---
+++ MJ2048/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MJ2048
+//
+//  Created by MJsaka on 8/26/15.
+//  Copyright (c) 2015 MJsaka. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ MJ2048/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  MJ2048
+//
+//  Created by MJsaka on 8/26/15.
+//  Copyright (c) 2015 MJsaka. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
